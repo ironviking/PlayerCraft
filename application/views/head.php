57bba780d7c5172a25f3cf7ef8d26195aeea0228
ironviking <kvnforsell@gmail.com>
2012-09-22 10:56:42 +0200

    Cleaned it up a bit, removed javascript in header

--- application/views/head.php
+++ application/views/head.php
@@ -1,22 +1,20 @@
+<?php /*Set base_url()*/ $url = base_url(); ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html LANG="EN">
+<!-- Head -->	
 <head>
     <title><?=$title?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
-	<link href="<?=base_url()?>css/style.css" rel="stylesheet">
+	<link href="<?=$url?>css/style.css" rel="stylesheet">
 	<link rel="icon" type="image/png" href="favicon.png">
 	<META NAME="Description" CONTENT="<?=$site_description?>">
     <link href='http://fonts.googleapis.com/css?family=Press+Start+2P' rel='stylesheet' type='text/css'>
-    <!-- Temporary Fix for IE problems -->
-   <SCRIPT language="JavaScript">
-   function fixie(){if (browserName=navigator.appName=="Microsoft Internet Explorer"){document.body.style.background = '#80B5FE';}}
-   </SCRIPT>
-
 </head>
-<body onload="fixie();">
-<script src="js/cst.js" type="javascript/text"></script>
+<!-- Body -->
+<body onload="fix();">
 	<div id="page">
 		<div id="logo">
-			<a href="<?=base_url()?>"><img src="<?=base_url()?>images/logo.png" alt="Logo"></a>
+			<a href="<?=$url?>"><img src="<?=$url?>images/logo.png" alt="Logo"></a>
 		</div>
 		<div id="container">
