b4638130b40f962fde982e5a4b5ec7bbcd2cd4cb
ironviking <kvnforsell@gmail.com>
2012-09-23 17:29:04 +0200

    Can no longer add pages with names that already exists

--- application/models/pagedb.php
+++ application/models/pagedb.php
@@ -68,6 +68,9 @@ class PageDB extends CI_Model {
 	
 	function addPage($page)
 	{
+			if($this->page_exists($page)){
+				return false;
+			}
 			$page = htmlentities(mysql_real_escape_string($page));
 			$this->db->query("INSERT INTO pages (`id`, `name`, `content`, `ord`, `status`) VALUES (NULL, '$page', '<p>Page content</p>', '10', '1');");
 	}
