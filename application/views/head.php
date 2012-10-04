b0842a08ef3c98b02ce365c6b57fbbee29c5c6f6
ironviking <kvnforsell@gmail.com>
2012-10-04 16:09:48 +0100

    Changed some tags to html5

--- application/views/head.php
+++ application/views/head.php
@@ -1,11 +1,11 @@
 <?php /*Set base_url()*/ $url = base_url(); ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!doctype html>
 <html LANG="EN">
 <!-- Head -->	
 <head>
     <title><?=$title?></title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+    <meta charset="UTF-8">
 	<link href="<?=$url?>css/style.css" rel="stylesheet">
 	<link rel="icon" type="image/png" href="images/favicon.png">
 	<META NAME="Description" CONTENT="<?=$site_description?>">
