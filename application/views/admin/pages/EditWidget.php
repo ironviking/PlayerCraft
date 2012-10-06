bb27ebab9a7d20ead54c58d17ff5d20f5cbeb4ba
ironviking <kvnforsell@gmail.com>
2012-10-06 12:23:11 +0200

    Fixed some styling

--- application/views/admin/pages/EditWidget.php
+++ application/views/admin/pages/EditWidget.php
@@ -11,7 +11,7 @@ foreach($widget->result() as $wid){
 	<hr>
 	<div id="editwidget">
 	<form method="post" action="">
-		<textarea cols="75" style="resize: none;" rows="10" name="content"><?=$widget_prop['content']?></textarea>
+		<textarea cols="75" style="resize: none; width: 625px;" rows="10" name="content"><?=$widget_prop['content']?></textarea>
 		<div class="mtop"><label>Title: </label><input name="title" required type="text" value="<?=$widget_prop['title']?>"><label> Order: </label><input name="order" required  type="number" value="<?=$widget_prop['order']?>"><br><br><input class="button" type="submit" value="save"> <a href="<?=base_url()?>admin/widgets"><input type="button" value="Cancel"></a></div>
 	</form>
 	</div>
