27f3201bf4221fdd2571bf6d7b6e5c1863aa616d
ironviking <kvnforsell@gmail.com>
2014-06-22 01:59:42 +0200

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,39 +1,42 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+// Handle admin login for administral operations
 class Login extends CI_Controller {
-
 	public function index(){
-
+		// If user already have admin, redirect to admin panel
 		if(isset($_SESSION['admin'])) {
 			header('location: ' .base_url() . 'admin');
 		}
-
+		
+		// Avoid empty data warning
+		$data[''] = "";
+		
+		// Retrieve user data from configuration
 		$this->load->config('user');
 		$conf_username = $this->config->item('username');
 		$conf_password = $this->config->item('password');
+		
+		// Make sure both fields have valid values
 		if($conf_username == "" or $conf_password == ""){
-            $data['fail'] = "You may not login until password and/or username has been set";
-        }else{
-
-		$data[''] = "";
-		if(isset($_POST['username'])) {
-
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-
-			if( $username == $conf_username && $password = $conf_password){
-				$_SESSION['admin'] = true;
-				$_SESSION['user'] = htmlentities(mysql_real_escape_string(stripslashes($_POST['username'])));
-				header('location: ' . base_url() . 'admin');
-
-			}else {
-				$data['fail'] = "password and/or username incorrect";
-			}
-
-        }
-
-        }
-
+            		$data['fail'] = "Invalid user configuration";
+        	} else {
+			if(isset($_POST['username'])) {
+				$username = $_POST['username'];
+				$password = $_POST['password'];
+				
+				// Check input towards configuration
+				if( $username == $conf_username && $password == $conf_password) {
+					$_SESSION['admin'] = true;
+					$_SESSION['user'] = htmlentities(mysql_real_escape_string(stripslashes($_POST['username'])));
+					header('location: '.base_url().'admin');
+				}
+				
+				$data['fail'] = "Incorrect user data ";
+				
+        		}		
+        	}
+		
+		// Load template
 		$this->load->view('login-template', $data);
 	}
 
